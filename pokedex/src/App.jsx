44d612c3bd07b1f64a900e94e9d925f8c52dc5b5
Devtrotter <sebastienchapuy19@gmail.com>
2023-11-24 23:01:45 +0100

    add multiple fetch

--- pokedex/src/App.jsx
+++ pokedex/src/App.jsx
@@ -1,38 +1,54 @@
 
-    import React, { useState, useEffect } from "react";
+    import { useState, useEffect } from "react";
 
 
     function App() { 
       
         const [pokemon, setPokemon] = useState('');
+        const [pokemons, setPokemons] = useState([]);
 
         useEffect(() => {
           const fetchPokemonSpecies = async () => {
-            fetch('https://pokeapi.co/api/v2/pokemon-species/', {
+            fetch('https://pokeapi.co/api/v2/pokemon/', {
               method: "GET",
             
             },
           )
             .then((response) => response.json())
             .then((data) => {
-              return data
+              setPokemon(data.results)
             });
           }
 
-          const arrayPokemon = fetchPokemonSpecies();
-          console.log(arrayPokemon);
+          fetchPokemonSpecies();
       }, []);
+
+      useEffect(() => {
+        const fetchPromise = pokemon && pokemon.map(({url}) => fetch(url).then((res) => res.json()));
+
+        Promise.all(fetchPromise)
+        .then(responses => {
+            const responseData = responses.map(response => response);
+            const pokemonData =  responseData.map(item => {
+                return {
+                    name: item.name,
+                    image: item.sprites.other.dream_world.front_default
+                }
+            });
+            setPokemons(pokemonData);
+        })
+        
+      }, [pokemon]);
       return (
         <div>
-          {pokemon && pokemon.map((pokemonEntitie) => <h1> {pokemonEntitie.name} </h1>)}
-          <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="pokemon" />
+          {pokemons && pokemons.map((pokemonEntitie) =>
+            <div key={pokemonEntitie.name}>
+              <h1>{pokemonEntitie.name}</h1>
+              <img src={pokemonEntitie.image} alt={pokemonEntitie.name} />
+            </div>)}
           <h2>Pokedex Max</h2>
         </div>
       );
     }
     export default App;
-    // const [images , setImages] = useState('');
-    //useEffect(() => {
-    // fetch( 'https://pokeapi.co/api/v2/pokemon/1/'  , {
-    // method: "GET",
       
